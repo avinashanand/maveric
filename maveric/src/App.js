9a8f9f52a15389ca8ae90af4476b44058c713769
Shweta <shweta.swarnima@gmail.com>
2023-01-23 23:35:30 +0530

    First commit for React App

---
+++ maveric/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Catalog from './component/Catalog.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <Catalog></Catalog>
+  );
+}
+
+export default App;
---
+++ maveric/src/component/Catalog.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import '../App.css';
+
+import axios from 'axios';
+import { maxWidth } from "@mui/system";
+export default function Catalog(props){
+    const [items, setItems] = useState([]);
+    const test = ["apple", "mango", "banana"];
+    useEffect(() =>{
+        axios.get('http://localhost:8080/catalog/all').then(res => {
+            setItems(res.data);
+        })
+    },[]);
+    return(
+        <>
+         <h1>Vege Express ... </h1>
+        <div className="float-container">
+            {items.map((item, index) => (
+                <Card sx={{ minWidth: 275, maxWidth:300 }} className="float-child">
+                    <CardContent>
+                        <CardMedia
+                            component="img"
+                            alt={item.name}
+                            height="200"
+                            image={item.image}
+                            title="Contemplative Reptile"
+                        />
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {item.name}
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        {item.description}
+                        </Typography>
+                        
+                        <Typography variant="body2">
+                        {item.category} in {item.subCategory}
+                        </Typography>
+                    </CardContent>
+                <CardActions>
+                  <Button size="small">Add to Cart</Button>
+                </CardActions>
+              </Card>
+            )
+                
+            )}
+        </div>
+        </>
+       
+    );
+}
